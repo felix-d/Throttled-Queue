f7b3479ff272d213a86816ccff9c2e61915ee32d
Felix Descoteaux <flx.descoteaux@gmail.com>
2017-10-30 13:01:12 -0400

    Updated mix.exs

--- mix.exs
+++ mix.exs
@@ -4,12 +4,23 @@ defmodule ThrottledQueue.Mixfile do
   def project do
     [
       app: :throttled_queue,
+      package: package(),
+      description: "A simple throttled queue built on top of GenServer with status updates.",
       version: "0.1.0-dev",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       name: "ThrottledQueue",
       source_url: "https://www.github.com/felix-d/throttled_queue",
+    ]
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Félix Descôteaux"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/felix-d/throttled_queue"}
     ]
   end
 
